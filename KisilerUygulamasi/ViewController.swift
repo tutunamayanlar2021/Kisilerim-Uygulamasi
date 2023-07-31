43cc2bde05cd0faa93945f4a28fd32e5c4082050
tutunamayanlar2021 <kaderoral2147@gmail.com>
2023-07-31 19:15:00 +0300

    Initial Commit

---
+++ KisilerUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KisilerUygulamasi
+//
+//  Created by Kader Oral on 31.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
